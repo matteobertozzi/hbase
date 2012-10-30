44386ac3d05b9162a3baefba492094229a39aebc
Matteo Bertozzi <matteo.bertozzi@cloudera.com>
2012-10-30 23:16:44 +0100

    Reference Helpers

--- hbase-server/src/main/java/org/apache/hadoop/hbase/io/Reference.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/io/Reference.java
@@ -1,5 +1,4 @@
 /**
- *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -24,7 +23,11 @@ import java.io.DataInputStream;
 import java.io.DataOutput;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.classification.InterfaceAudience;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
@@ -57,8 +60,19 @@ import com.google.protobuf.ByteString;
  */
 @InterfaceAudience.Private
 public class Reference implements Writable {
+  private static final Log LOG = LogFactory.getLog(Reference.class);
   private byte [] splitkey;
   private Range region;
+  /**
+   * Regex that will work for straight filenames and for reference names. If reference, then the
+   * regex has more than just one group. Group 1 is this files id. Group 2 the referenced region
+   * name, etc.
+   * <p>
+   * For instance, "fd1e73e8a96c486090c5cec07b4894c4.test", where everything before the "." is the
+   * name of the original reference file and everything after is the table/region name.
+   */
+  public static final Pattern REF_OR_HFILE_NAME_PARSER = Pattern
+      .compile("^([0-9a-f]+)(?:\\.(.+))?$");
 
   /**
    * For split HStoreFiles, it specifies if the file covers the lower half or
@@ -92,7 +106,7 @@ public class Reference implements Writable {
    * @param splitRow This is row we are splitting around.
    * @param fr
    */
-  Reference(final byte [] splitRow, final Range fr) {
+  Reference(final byte[] splitRow, final Range fr) {
     this.splitkey = splitRow == null?  null: KeyValue.createFirstOnRow(splitRow).getKey();
     this.region = fr;
   }
@@ -160,6 +174,9 @@ public class Reference implements Writable {
     FSDataOutputStream out = fs.create(p, false);
     try {
       out.write(toByteArray());
+    } catch (IOException e) {
+      LOG.error("Failed to write reference: " + p, e);
+      throw e;
     } finally {
       out.close();
     }
@@ -201,7 +218,7 @@ public class Reference implements Writable {
     }
   }
 
-  FSProtos.Reference convert() {
+  public FSProtos.Reference convert() {
     FSProtos.Reference.Builder builder = FSProtos.Reference.newBuilder();
     builder.setRange(isTopFileRegion(getFileRegion())?
       FSProtos.Reference.Range.TOP: FSProtos.Reference.Range.BOTTOM);
@@ -209,7 +226,7 @@ public class Reference implements Writable {
     return builder.build();
   }
 
-  static Reference convert(final FSProtos.Reference r) {
+  public static Reference convert(final FSProtos.Reference r) {
     Reference result = new Reference();
     result.splitkey = r.getSplitkey().toByteArray();
     result.region = r.getRange() == FSProtos.Reference.Range.TOP? Range.top: Range.bottom;
@@ -225,5 +242,58 @@ public class Reference implements Writable {
    */
   byte [] toByteArray() throws IOException {
     return ProtobufUtil.prependPBMagic(convert().toByteArray());
+  }
+
+  /**
+   * @param m Matcher constructed around a file name (should be based on
+   *          {@link #REF_OR_HFILE_NAME_PARSER}).
+   * @return <tt>true</tt> if it meets the basic requirements of a reference file
+   */
+  private static boolean getMatcherFoundReference(Matcher m) {
+    return m.groupCount() > 1 && m.group(2) != null;
+  }
+
+  /**
+   * @param p Path to check.
+   * @param m Matcher to use.
+   * @return True if the path has format of a HStoreFile reference.
+   * @throws RuntimeException if the store file name doesn't match
+   */
+  public static boolean isReference(final Path p, final Matcher m) {
+    LOG.debug("Checking to see if " + p + " is a reference.");
+    if (m == null || !m.matches()) {
+      LOG.warn("Failed match of store file name " + p.toString());
+      return false;
+    }
+    return getMatcherFoundReference(m);
+  }
+
+  /**
+   * @param p Path to check.
+   * @return <tt>true</tt> if the path has format of a HStoreFile reference.
+   * @throws RuntimeException if the path is not a reference. Use {@link #checkReference(Path)} if
+   *           you want the non-throwing version.
+   */
+  public static boolean isReference(final Path p) {
+    return !p.getName().startsWith("_")
+        && Reference.isReference(p, Reference.REF_OR_HFILE_NAME_PARSER.matcher(p.getName()));
+  }
+
+  /**
+   * Get the original HFile name
+   * @param fileName name of the file to get the original HFile Id. Can either be the name of an
+   *          HFile or a reference to the file
+   * @return Original name of the (possibly referenced) hfile
+   * @throws IllegalArgumentException if the file is not an Hfile or a reference file
+   */
+  public static String getDeferencedHFileName(String fileName) {
+    Matcher m = Reference.REF_OR_HFILE_NAME_PARSER.matcher(fileName);
+    if (!m.matches()) throw new IllegalArgumentException(
+        "Not a valid reference file or hfile name!");
+    // otherwise, it matches
+    // if it is a reference, then get the original file
+    if (getMatcherFoundReference(m)) return m.group(1);
+    // if it matches, but isn't a reference file, then its just an hfile
+    return fileName;
   }
 }
